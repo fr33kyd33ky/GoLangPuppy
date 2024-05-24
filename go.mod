1fabb9ee37d674be23135262d836c24439635b72
Christian McMillan <christian.mcmillan@q2.com>
2024-05-24 10:15:47 -0500

    Add stuff

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/fr33kyd33ky/GoLangPuppy
+
+go 1.22.3
+
+require github.com/fr33kyd33ky/GoLangDog v0.0.0-20240524150905-e6a7d77ae317 // indirect
--- golangpuppy.go
+++ golangpuppy.go
@@ -1,9 +1,17 @@
 package golangpuppy
 
+import (
+	golangdog "github.com/fr33kyd33ky/GoLangDog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return golangdog.WhenGrownUp(Bark())
 }
